7e95e0de60cd049beeb8b694397945d28f95011b
Johny Vallejo-Sánchez <35435216+Hausdorff94@users.noreply.github.com>
2023-07-27 05:43:23 +0000

    Add app.py script with a basic function

---
+++ src/utils/app.py
@@ -0,0 +1,18 @@
+def sum_function(a: float, b: float) -> float:
+    '''
+    Sum two numbers
+
+    Parameters
+    ----------
+    a : float
+        First number
+    b : float
+        Second number
+
+    Returns
+    -------
+    float
+        Sum of a and b
+
+    '''
+    return a+b
